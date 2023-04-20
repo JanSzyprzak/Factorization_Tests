90064b306187f91e1d1d3c9a702a744dab5c7d2f
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-20 12:45:05 +0200

    Addtest if prime_factor() returns factors_list

--- test_factorization.py
+++ test_factorization.py
@@ -1,8 +1,11 @@
 from main import prime_factors  
 
 def test_import_prime_factors():
-    try:
-        
+    try: 
+
         assert callable(prime_factors), "prime_factors not callable"  
     except ImportError as error:  
         assert False, error
+
+def test_if_f_returns_factors_list():
+    assert type(prime_factors(127)) == list
