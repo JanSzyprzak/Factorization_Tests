0ce172d91f4062d4f5a55b963fd2245f1cb44d26
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-20 13:32:05 +0200

    Add test for input validation

--- test_factorization.py
+++ test_factorization.py
@@ -1,11 +1,23 @@
 from main import prime_factors  
 
+
 def test_import_prime_factors():
     try: 
-
-        assert callable(prime_factors), "prime_factors not callable"  
+        assert callable(prime_factors), "prime_factors is not callable"  
     except ImportError as error:  
         assert False, error
 
 def test_if_f_returns_factors_list():
     assert type(prime_factors(127)) == list
+
+
+def test_p_f_input_validation():    
+    
+    invalid_inputs = ["hello", 3.14, True, [2, 3, 4]]
+
+    for invalid_input in invalid_inputs:
+        try:
+            prime_factors(invalid_input)
+            assert False, "Function should raise TypeError when called with non-integer argument"
+        except TypeError as e:
+            assert str(e) == "Input must be an integer"
