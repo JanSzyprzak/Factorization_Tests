aaa86e3d7a20a508366a7db9223d78cfe24432c2
Jan Szyprzak <jan.szyprzak@gmail.com>
2023-04-21 11:59:36 +0200

    Factorize 2

--- test_factorization.py
+++ test_factorization.py
@@ -11,13 +11,5 @@ def test_if_f_returns_factors_list():
     assert type(prime_factors(127)) == list
 
 
-def test_p_f_input_validation():    
-    
-    invalid_inputs = ["hello", 3.14, True, [2, 3, 4]]
-
-    for invalid_input in invalid_inputs:
-        try:
-            prime_factors(invalid_input)
-            assert False, "Function should raise TypeError when called with non-integer argument"
-        except TypeError as e:
-            assert str(e) == "Input must be an integer"
+def test_p_f_2():
+    assert prime_factors(2) == [2]
